de0ed65b6a70e33fc47f66da185ec1d707ef84be
CardinalJV <jessy.viranaiken@icloud.com>
2024-10-15 17:44:35 +0200

    Initial Commit

---
+++ iReminder/iReminderApp.swift
@@ -0,0 +1,17 @@
+//
+//  iReminderApp.swift
+//  iReminder
+//
+//  Created by Jessy Viranaiken on 15/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct iReminderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
