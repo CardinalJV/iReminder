0235a1a7efffe68dac21d61130f73a88367c9d5a
CardinalJV <jessy.viranaiken@icloud.com>
2024-10-29 18:47:05 +0100

    Update: UI/UX

--- iReminder/Components/TaskItem.swift
+++ iReminder/Components/TaskItem.swift
@@ -10,30 +10,94 @@ import SwiftUI
 struct TaskItem: View {
   
   let taskController: TaskController
-  let task: TaskModel
-  @State var isCompleted = false
+  @State var task: TaskModel
   
-  var body: some View {
-    HStack {
-      Text(task.name)
-      Spacer()
-      Button(action: {
-        self.isCompleted = true
-        if let context = taskController.context {
-          DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
-            task.isCompleted = true
-            try? context.save()
+  @State private var viewOffset: CGFloat = 0
+  @State private var showRemoveButton = false
+  @State private var removeRow = false
+  @State private var isEditing = false
+  @State private var text = ""
+  @FocusState private var isTextFieldFocused: Bool
+  
+  let baseOffset: CGFloat = 0
+  let deleteOffset: CGFloat = -40
+  
+  var dragGesture: some Gesture {
+    DragGesture(minimumDistance: 3.0, coordinateSpace: .local)
+      .onEnded { value in
+        if value.translation.width < 0 {
+          withAnimation(.bouncy(duration: 0.30)) {
+            self.viewOffset = deleteOffset
+            self.showRemoveButton = true
+          }
+        } else if value.translation.width > 0 {
+          withAnimation(.bouncy(duration: 0.30)) {
+            self.viewOffset = baseOffset
+            self.showRemoveButton = false
           }
         }
-      }) {
-        Image(systemName: self.isCompleted  ? "checkmark.circle.fill" : "circle")
-          .resizable()
-          .frame(width: 24, height: 24)
+      }
+  }
+  
+  var body: some View {
+    HStack(spacing: 0) {
+      ZStack{
+        /* Shape shadow */
+        RoundedRectangle(cornerRadius: 8)
+          .offset(x: 4, y: 6)
+        RoundedRectangle(cornerRadius: 8)
+          .foregroundColor(.background)
+        /* - */
+        HStack(spacing: 8){
+          /* Checkmark button */
+          Button {
+            self.task.isCompleted.toggle()
+            self.taskController.sortTasks()
+          } label: {
+            ZStack{
+              RoundedRectangle(cornerRadius: 5)
+                .stroke(.black, lineWidth: 5)
+                .fill(self.task.isCompleted ? .black : .white)
+                .animation(self.task.isCompleted ? .easeOut(duration: 0.3) : .default, value: self.task.isCompleted)
+                .frame(width: 30, height: 30)
+              if (self.task.isCompleted) {
+                Image(systemName: "checkmark")
+                  .foregroundStyle(.white)
+                  .frame(width: 24, height: 24)
+                  .bold()
+                  .animation(.easeInOut(duration: 0.25), value: self.task.isCompleted)
+              }
+            }
+          }
+          .buttonStyle(.plain)
+          /* - */
+          /* Task name field */
+          CustomTextWrapper(task: self.task)
+          /* - */
+          Spacer()
+        }
+        .padding(14)
+        RoundedRectangle(cornerRadius: 8)
+          .stroke(style: StrokeStyle(lineWidth: 2.5, lineCap: .round, lineJoin: .round))
+      }
+      .offset(x: viewOffset)
+      .fixedSize(horizontal: false, vertical: true)
+      .padding(.horizontal, 24)
+      .gesture(dragGesture)
+      if self.showRemoveButton {
+        Button {
+          self.taskController.delete(this: self.task)
+        } label: {
+          Image(systemName: "multiply")
+            .font(.largeTitle)
+            .bold()
+        }
+        .padding()
+        .buttonStyle(.plain)
+        .transition(.scale.animation(.bouncy))
+        .offset(x: -34)
+        .frame(width: 32, height: 32)
       }
     }
-    .padding()
-    .frame(height: 45)
-    .background(.white)
-    .cornerRadius(10)
   }
 }
