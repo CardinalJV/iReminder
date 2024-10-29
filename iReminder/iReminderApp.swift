0a50655c798f345d1f94335fdf9b049d0936d401
CardinalJV <jessy.viranaiken@icloud.com>
2024-10-29 18:53:51 +0100

    Update: UI/UX

--- iReminder/iReminderApp.swift
+++ iReminder/iReminderApp.swift
@@ -12,7 +12,7 @@ import SwiftData
 struct iReminderApp: App {
     var body: some Scene {
         WindowGroup {
-            LandingView()
+          LandingView()
         }
         .modelContainer(for: TaskModel.self)
     }
