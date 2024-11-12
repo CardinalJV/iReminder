29e289e5275446d825184a94c4473586220a670f
CardinalJV <jessy.viranaiken@icloud.com>
2024-11-12 18:14:18 +0100

    Update: UI/UX

--- Sources/iReminderApp.swift
+++ Sources/iReminderApp.swift
@@ -1,19 +1,19 @@
-//
-//  iReminderApp.swift
-//  iReminder
-//
-//  Created by Jessy Viranaiken on 15/10/2024.
-//
+  //
+  //  iReminderApp.swift
+  //  iReminder
+  //
+  //  Created by Jessy Viranaiken on 15/10/2024.
+  //
 
 import SwiftUI
 import SwiftData
 
 @main
 struct iReminderApp: App {
-    var body: some Scene {
-        WindowGroup {
-          LandingView()
-        }
-        .modelContainer(for: TaskModel.self)
+  var body: some Scene {
+    WindowGroup {
+      LandingView()
     }
+    .modelContainer(for: TaskModel.self)
+  }
 }
